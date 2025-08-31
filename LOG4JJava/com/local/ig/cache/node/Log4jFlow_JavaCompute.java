d2f306edb603d544146e3049f6762a3bbd64b34b
Aditya <adityanarayanchaudhary8@gmail.com>
2025-08-31 19:45:03 +0530

    This is my first personal project

---
+++ LOG4JJava/com/local/ig/cache/node/Log4jFlow_JavaCompute.java
@@ -0,0 +1,128 @@
+package com.local.ig.cache.node;
+
+import com.ibm.broker.javacompute.MbJavaComputeNode;
+import com.ibm.broker.plugin.MbException;
+import com.ibm.broker.plugin.MbMessage;
+import com.ibm.broker.plugin.MbMessageAssembly;
+import com.ibm.broker.plugin.MbOutputTerminal;
+import com.ibm.broker.plugin.MbUserException;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Log4jFlow_JavaCompute extends MbJavaComputeNode {
+
+	public void evaluate(MbMessageAssembly inAssembly) throws MbException {
+		MbOutputTerminal out = getOutputTerminal("out");
+		MbOutputTerminal alt = getOutputTerminal("alternate");
+
+		MbMessage inMessage = inAssembly.getMessage();
+		MbMessageAssembly outAssembly = null;
+		final Logger DOWNSTREAM = LogManager.getLogger(Log4jFlow_JavaCompute.class);
+		try {
+			// Log messages at different levels
+			DOWNSTREAM.info("Starting message processing for a new message.");
+            
+         // Example of getting message content and logging it
+            String messagePayload = new String(inMessage.getRootElement().getLastChild().toString());
+            
+         // Use debug for more detailed information
+            DOWNSTREAM.debug("Received payload: " + messagePayload);
+            
+         // Example of a conditional log for a specific scenario
+            if (messagePayload.length() > 100) {
+            	DOWNSTREAM.warn("Message size exceeds 100 characters. Consider optimization.");
+            }
+            
+         // Log the completion of the process
+            DOWNSTREAM.info("Message processing completed successfully.");
+            
+			// create new message as a copy of the input
+			MbMessage outMessage = new MbMessage(inMessage);
+			outAssembly = new MbMessageAssembly(inAssembly, outMessage);
+			getOutputTerminal("out").propagate(outAssembly);
+			// ----------------------------------------------------------
+			// Add user code below
+
+			// End of user code
+			// ----------------------------------------------------------
+		} catch (MbException e) {
+			DOWNSTREAM.error("An IBM ACE-specific exception occurred.", e);
+            throw e; // Re-throw to ensure the flow handles the exception
+
+		} catch (RuntimeException e) {
+			// Re-throw to allow Broker handling of RuntimeException
+			throw e;
+		} catch (Exception e) {
+			// Consider replacing Exception with type(s) thrown by user code
+			// Example handling ensures all exceptions are re-thrown to be handled in the flow
+			DOWNSTREAM.error("A generic exception occurred in the flow.", e);
+            //throw new MbException(e, "Exception in MyLog4jNode");
+		}
+		// The following should only be changed
+		// if not propagating message to the 'out' terminal
+		out.propagate(outAssembly);
+
+	}
+
+	/**
+	 * onPreSetupValidation() is called during the construction of the node
+	 * to allow the node configuration to be validated.  Updating the node
+	 * configuration or connecting to external resources should be avoided.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onPreSetupValidation() throws MbException {
+	}
+
+	/**
+	 * onSetup() is called during the start of the message flow allowing
+	 * configuration to be read/cached, and endpoints to be registered.
+	 *
+	 * Calling getPolicy() within this method to retrieve a policy links this
+	 * node to the policy. If the policy is subsequently redeployed the message
+	 * flow will be torn down and reinitialized to it's state prior to the policy
+	 * redeploy.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onSetup() throws MbException {
+	}
+
+	/**
+	 * onStart() is called as the message flow is started. The thread pool for
+	 * the message flow is running when this method is invoked.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onStart() throws MbException {
+	}
+
+	/**
+	 * onStop() is called as the message flow is stopped. 
+	 *
+	 * The onStop method is called twice as a message flow is stopped. Initially
+	 * with a 'wait' value of false and subsequently with a 'wait' value of true.
+	 * Blocking operations should be avoided during the initial call. All thread
+	 * pools and external connections should be stopped by the completion of the
+	 * second call.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onStop(boolean wait) throws MbException {
+	}
+
+	/**
+	 * onTearDown() is called to allow any cached data to be released and any
+	 * endpoints to be deregistered.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onTearDown() throws MbException {
+	}
+
+}
